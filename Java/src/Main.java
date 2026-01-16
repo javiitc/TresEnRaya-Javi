0c6f3ad147ca5d4af5206fb0337dc0062188e6c8
Javi <itsjavii1610@gmail.com>
2026-01-16 13:40:41 +0100

    pj

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -3,5 +3,16 @@ import java.util.Scanner;
 public class Main {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("======== Tres en raya ========");
+        System.out.println("Indica el nombre de los jugadores:");
+        String jugador1 = sc.next();
+        String jugador2 = sc.next();
+
+        int filas = 3;
+        int columnas = 3;
+        int [][] tablero = new int[filas][columnas];
+
+        
     }
 }
